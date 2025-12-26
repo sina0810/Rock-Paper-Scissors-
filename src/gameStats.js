53f933ca087df3c091438c4124b0a5792ec4d7f8
Marcy Student <Marcy_Student@MARCY-MBAir40.local>
2025-12-26 08:34:50 -0500

    Added third file called (gameStats)

---
+++ src/gameStats.js
@@ -0,0 +1,7 @@
+const gameStats = {
+    wins: 0,
+    losses: 0,
+    ties: 0
+};
+
+module.exports = {gameStats};
--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,28 @@
+const { gameStats } = require('./gameStats.js')
+
 const prompt = require('prompt-sync')();
 
-const showMenu = () => {
-  console.log("Menu:");
-  console.log("1. [First Option]");
-  console.log("2. [Second Option]");
-  console.log("3. Exit");
+const showMenu = () => {     
+  let isRunning = true;
+  while (isRunning) {
+    console.log("\nMenu:");
+    console.log("1. [First Option]");
+    console.log("2. [Second Option]");
+    console.log("3. Exit");
 
-  const choice = prompt("Choose an option: ");
-  console.log(`You chose: ${choice}`);
+    const choice = prompt("Choose an option: ");
+
+    if (choice === "1") {
+      console.log("You chose option 1");
+    } else if (choice === "2") {
+      console.log("You chose option 2");
+    } else if (choice === "3") {
+      console.log("Goodbye!");
+      isRunning = false;
+    } else {
+      console.log("Invalid choice. Please try again.");
+    }
+  }
 };
 
 module.exports = { showMenu };
